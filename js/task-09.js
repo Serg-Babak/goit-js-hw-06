0f9daa6172845135b788a65b2f7bfb042b61ee14
Serg-Babak <babakserg1@gmail.com>
2022-11-13 22:34:47 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.querySelector("body");
+const btn = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+
+const changeColor = (event ) =>{
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+};
+
+btn.addEventListener("click", changeColor);
